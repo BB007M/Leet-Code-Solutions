bf9ef7804cd6f847a21a42f133e064aa7704640c
BARATH MITHUN B <barathmithun10@gmail.com>
2026-09-19 12:17:44 +0530

    Time: 0 ms (100%), Space: 48.4 MB (10.67%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,27 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n; // Use long to handle Integer.MIN_VALUE overflow
+        
+        // Handle negative exponents
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        
+        double result = 1.0;
+        double currentProduct = x;
+        
+        // Binary Exponentiation
+        while (N > 0) {
+            // If the current power is odd, multiply the result by currentProduct
+            if (N % 2 == 1) {
+                result *= currentProduct;
+            }
+            // Square the base and halve the exponent
+            currentProduct *= currentProduct;
+            N /= 2;
+        }
+        
+        return result;
+    }
+}
